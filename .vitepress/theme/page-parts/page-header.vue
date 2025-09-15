2a1652218179e8ab2dd82639c5196938df56fbf4
zawa-ch. <satellite.2e1834097@gmail.com>
2025-09-15 16:28:48 +0900

    特定のドキュメントのヘッダのレンダリング失敗を修正

--- .vitepress/theme/page-parts/page-header.vue
+++ .vitepress/theme/page-parts/page-header.vue
@@ -11,7 +11,8 @@
 		<div v-if="willApplicateArticle" class="custom-block info">
 			<p class="custom-block-title">現在表示しているのは今後新しく発効する予定の版です。</p>
 			<p>
-				この文書は <DateTip :date="applicableDate" fuzzyness="minute" /> に発効する版です。
+				この文書は
+				<DateTip :date="applicableDate" fuzzyness="minute" /> に発効する版です。
 			</p>
 			<p v-if="typeof frontmatter.historyPath === 'string'">
 				現在有効な版は、<a :href="frontmatter.historyPath">更新履歴</a>から確認できます。
@@ -20,16 +21,18 @@
 		<div v-if="willObsoleteArticle" class="custom-block warning">
 			<p class="custom-block-title">現在表示しているのは過去の版です。</p>
 			<p>
-				この文書は <DateTip :date="obsoleteDate" fuzzyness="minute" /> に失効する版です。
+				この文書は
+				<DateTip :date="obsoleteDate" fuzzyness="minute" /> に失効する版です。
 			</p>
 			<p v-if="typeof frontmatter.obsolete.applicablePath === 'string'">
 				<a :href="frontmatter.obsolete.applicablePath">今後有効になる版はこちら</a>から確認できます。
 			</p>
 		</div>
-		<div v-if="isObsoletedArticle" class="custom-block danger">
+		<div v-else-if="isObsoletedArticle" class="custom-block danger">
 			<p class="custom-block-title">現在表示しているのは過去の版です。</p>
 			<p>
-				この文書は <DateTip :date="obsoleteDate" fuzzyness="minute" /> に失効した過去の版です。
+				この文書は
+				<DateTip :date="obsoleteDate" fuzzyness="minute" /> に失効した過去の版です。
 				参考のために残されており、これがなんらかの効力を発することはないことに注意してください。
 			</p>
 			<p v-if="typeof frontmatter.obsolete.applicablePath === 'string'">
@@ -47,15 +50,15 @@ import { computed } from 'vue';
 const { frontmatter } = useData();
 const pageDate = computed(() => parseDate(frontmatter.value.date))
 const applicableDate = computed(() => parseDate(frontmatter.value.applicableDate))
-const obsoleteDate = computed(()=>{
-	if (typeof frontmatter.value.obsolete !== 'object') { return undefined; }
+const obsoleteDate = computed(() => {
+	if (!frontmatter.value.obsolete || typeof frontmatter.value.obsolete !== 'object') { return undefined; }
 	return parseDate(frontmatter.value.obsolete.date);
 })
-const willApplicateArticle = computed(()=> {
+const willApplicateArticle = computed(() => {
 	if (applicableDate.value === undefined) { return false; }
 	return applicableDate.value > Date.now();
 })
-const willObsoleteArticle = computed(()=> {
+const willObsoleteArticle = computed(() => {
 	if (obsoleteDate.value === undefined) { return false; }
 	return obsoleteDate.value > Date.now();
 })
@@ -64,7 +67,7 @@ const isObsoletedArticle = computed(() => {
 	return obsoleteDate.value <= Date.now();
 })
 
-function parseDate(d: unknown) {
+function parseDate(d: unknown): number | undefined {
 	if (typeof d === 'number') {
 		return d;
 	} else if (typeof d === 'string') {
